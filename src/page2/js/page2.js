8eb36f9354938ca433131e5f832b0235e35f610b
Kyle Gaither <30640336+KyleGaither@users.noreply.github.com>
2023-05-28 21:51:24 -0400

    initial baseline of multi-page bootstrap with vite app.

---
+++ src/page2/js/page2.js
@@ -0,0 +1,4 @@
+import '../scss/page2styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: path.resolve(__dirname, 'src'),
+    build: {
+      rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        page2: resolve(__dirname, 'src/page2/index.html'),
+      },
+    },
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080,
+    hot: true
+  }
+})
