edda1f97801d582f4d273ea84999d6dcfd2a45df
Kyle Gaither <30640336+KyleGaither@users.noreply.github.com>
2023-06-10 18:12:59 -0400

    - use item type selection to hide element on initial load and on change.

---
+++ frontend/dist/assets/design-78e954d1.js
@@ -0,0 +1,1 @@
+import"./bootstrap.esm-40bbabb8.js";let t="hello from design.js!",n=document.getElementById("resetButton");n.addEventListener("click",o(t));function o(e){console.log(e)}
--- frontend/src/designForm/js/design.js
+++ frontend/src/designForm/js/design.js
@@ -2,3 +2,42 @@ import '../scss/designStyles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+
+let message = "hello from design.js!";
+let resetbutton = document.getElementById("resetButton");
+let selectItemType = document.getElementById("selectItemType");
+
+
+function initialHide(){
+    let hiddenItemType = document.getElementById("hideDiv");
+    hiddenItemType.style.display= "none";
+}
+
+function hideDiv() {
+    var typeSelected = selectItemType.options[selectItemType.selectedIndex].text;
+
+    if(typeSelected == "Garment")
+    document.getElementById('hideDiv').style.display = "none";
+    else
+    document.getElementById('hideDiv').style.display = 'block';
+}
+
+
+function sayHello(message){
+    console.log(message);
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    initialHide();
+});
+
+selectItemType.addEventListener("change", function(){
+    hideDiv();
+});
+
+resetbutton.addEventListener('click', function(){
+    sayHello(message);
+});
