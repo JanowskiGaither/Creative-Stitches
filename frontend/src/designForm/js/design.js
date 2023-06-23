e0dd301b79713975334a8fb5748937bac3eed0c0
Michael Janowski <56001129+evernight2015@users.noreply.github.com>
2023-06-23 09:00:23 -0400

    Start support for multiple g arments

--- frontend/src/designForm/js/design.js
+++ frontend/src/designForm/js/design.js
@@ -9,14 +9,30 @@ let message = "hello from design.js!";
 let resetbutton = document.getElementById("resetButton");
 let previousGarmentbutton = document.getElementById("previousGarment");
 let nextGarmentbutton = document.getElementById("nextGarment");
+let garmentAmount = document.getElementById("garmentAmount");
+let garmentCostPerItem = document.getElementById("garmentCostPerItem");
+let otherAmount = document.getElementById("otherAmount");
+let otherCostPerItem = document.getElementById("otherCostPerItem");
 let selectItemType = document.getElementById("designType");
 
-function initialHide() {
+function initialSetup() {
+    // Hide Other initially
     let hiddenItemType = document.getElementById("showOther");
     hiddenItemType.style.display = "none";
+
+    // Calculate total costs at start
+    var otherTotal = document.getElementById('otherAmount').value * document.getElementById('otherCostPerItem').value;
+    document.getElementById('otherTotalCost').value = otherTotal;
+
+    var garmentTotal = document.getElementById('garmentAmount').value * document.getElementById('garmentCostPerItem').value;
+    document.getElementById('garmentTotalCost').value = garmentTotal;
+
+    //Assign initial GarmentID and OtherID to 1
+    document.getElementById('garmentID').value = 1;
+
 }
 
-function hideDiv() {
+function itemTypeSelection() {
     var typeSelected = selectItemType.options[selectItemType.selectedIndex].text;
 
     if (typeSelected == "Garment") {
@@ -33,14 +49,41 @@ function sayHello(message) {
     console.log(message);
 }
 
+function calculateOtherTotal() {
+    var otherTotal = document.getElementById('otherAmount').value * document.getElementById('otherCostPerItem').value;
+    document.getElementById('otherTotalCost').value = otherTotal;
+}
+
+function calculateGarmentTotal() {
+    var garmentTotal = document.getElementById('garmentAmount').value * document.getElementById('garmentCostPerItem').value;
+    document.getElementById('garmentTotalCost').value = garmentTotal;
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
-    initialHide();
+    initialSetup();
 });
 
 selectItemType.addEventListener("change", function () {
-    hideDiv();
+    itemTypeSelection();
 });
+
+garmentAmount.addEventListener("change", function () {
+    calculateGarmentTotal();
+});
+
+garmentCostPerItem.addEventListener("change", function () {
+    calculateGarmentTotal();
+});
+
+otherAmount.addEventListener("change", function () {
+    calculateOtherTotal();
+});
+
+otherCostPerItem.addEventListener("change", function () {
+    calculateOtherTotal();
+});
+
 
 resetbutton.addEventListener('click', function () {
     sayHello(message);
