8eb536e5f0fa38789945f53f8cf4977fdfaa338f
Michael Janowski <56001129+evernight2015@users.noreply.github.com>
2023-07-08 12:40:56 -0400

    Attempt to use sessionStorage for orderID - Not Working

--- frontend/src/designForm/js/design.js
+++ frontend/src/designForm/js/design.js
@@ -1,31 +1,47 @@
 import '../scss/designStyles.scss'
 
+const Promise = require('promise');
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-let message = "hello from design.js!";
-let resetbutton = document.getElementById("resetButton");
+// Create Garment Class
+class Garment {
+    designID;
+    orderID;
+    garmentID;
+    garmentGender;
+    garmentSize;
+    garmentStyleNumber;
+    garmentAmount;
+    garmentCostPerItem;
+    garmentTotalCost;
+    constructor() {
+
+    }
+}
+
+// Link commonly used variables from HTML
 let previousGarmentbutton = document.getElementById("previousGarment");
 let nextGarmentbutton = document.getElementById("nextGarment");
-let garmentAmount = document.getElementById("garmentAmount");
-let garmentCostPerItem = document.getElementById("garmentCostPerItem");
-let otherAmount = document.getElementById("otherAmount");
-let otherCostPerItem = document.getElementById("otherCostPerItem");
 let selectItemType = document.getElementById("designType");
-let otherTotalCost = document.getElementById("otherTotalCost");
-let garmentTotalCost = document.getElementById("garmentTotalCost");
+let orderID = document.getElementById("orderID");
+
 
 function initialSetup() {
     // Hide Other initially
     let hiddenItemType = document.getElementById("showOther");
     hiddenItemType.style.display = "none";
 
-    // Calculate total costs at start
-    var otherTotal = otherAmount.value * otherCostPerItem.value;
-    otherTotalCost.value = otherTotal;
+    //Retrieve orderID - Not currently working
+    orderID = sessionStorage.getItem('orderID');
 
-    var garmentTotal = garmentAmount.value * garmentCostPerItem.value;
-    garmentTotalCost.value = garmentTotal;
+    // Calculate total costs at start
+    var otherTotal = document.getElementById("otherAmount").value * document.getElementById("otherCostPerItem").value;
+    document.getElementById("otherTotalCost").value = otherTotal;
+
+    var garmentTotal = document.getElementById("garmentAmount").value * document.getElementById("garmentCostPerItem").value;
+    document.getElementById("garmentTotalCost").value = garmentTotal;
 
     //Assign initial GarmentID and OtherID to 1
     document.getElementById('garmentID').value = 1;
@@ -45,24 +61,43 @@ function itemTypeSelection() {
     }
 }
 
-function sayHello(message) {
-    console.log(message);
-}
-
+// Update Other Total Cost after user input
 function calculateOtherTotal() {
     var otherTotal = document.getElementById('otherAmount').value * document.getElementById('otherCostPerItem').value;
-    otherTotalCost.value = otherTotal;
+    document.getElementById("otherTotalCost").value = otherTotal;
 }
 
+// Update Garment Total Cost after user input
 function calculateGarmentTotal() {
     var garmentTotal = document.getElementById('garmentAmount').value * document.getElementById('garmentCostPerItem').value;
-    garmentTotalCost.value = garmentTotal;
+    document.getElementById("garmentTotalCost").value = garmentTotal;
 }
 
-function getNextGarment() {
-    var garmentTotal = document.getElementById('garmentAmount').value * document.getElementById('garmentCostPerItem').value;
-    garmentTotalCost.value = garmentTotal;
+// Save the current garment and retrieve the next garment's values to display
+async function nextGarment() {
+    // "/designSubmit" method="post"
+
+    //Retreive current garment values from HTML
+    currentGarment = new Garment();
+    currentGarment.designID = document.getElementById("designID").value;
+    currentGarment.orderID = document.getElementById("orderID").value;
+    currentGarment.garmentID = document.getElementById("garmentID").value;
+    currentGarment.garmentGender = document.getElementById("garmentGender").value;
+    currentGarment.garmentSize = document.getElementById("garmentSize").value;
+    currentGarment.garmentStyleNumber = document.getElementById("garmentStyleNumber").value;
+    currentGarment.garmentAmount = document.getElementById("garmentAmount").value;
+    currentGarment.garmentCostPerItem = document.getElementById("garmentCostPerItem").value;
+    currentGarment.garmentTotalCost = document.getElementById("garmentTotalCost").value;
+
+    fetch('/designSubmit', {
+        method: 'POST',
+        body: JSON.stringify(currentGarment)
+    })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
 }
+
+// Create event listeners to handle user inputs
 
 document.addEventListener("DOMContentLoaded", function () {
     initialSetup();
@@ -88,15 +123,10 @@ otherCostPerItem.addEventListener("change", function () {
     calculateOtherTotal();
 });
 
-
-resetbutton.addEventListener('click', function () {
-    sayHello(message);
-});
-
 previousGarmentbutton.addEventListener('click', function () {
 
 });
 
 nextGarmentbutton.addEventListener('click', function () {
-
+    nextGarment();
 });
