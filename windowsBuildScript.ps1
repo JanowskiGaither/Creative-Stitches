579cc2679fce94be57199ec77ca068781109bd10
Kyle Gaither <30640336+KyleGaither@users.noreply.github.com>
2023-08-28 21:19:54 -0400

    fix scroll bars on windows

--- windowsBuildScript.ps1
+++ windowsBuildScript.ps1
@@ -1,12 +1,9 @@
 Set-Location ./frontend
 
-# Copy the Library folder
-# For now its just generic .js files
-.\copyLibrary.ps1
-
 npm run build
 
 Set-Location ..
+
 docker build -t taxapp-frontend ./frontend
 docker build -t taxapp-backend ./backend
 docker compose up
